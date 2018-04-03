1a1a374ce612d768bc65ee8e3a33d6a0aa7b9357
Cody Mann <mann53@purdue.edu>
2018-04-03 11:03:26 -0400

    changed the rest of the led functions

--- MiniProjectDraft2/Src/ledFunctions.c
+++ MiniProjectDraft2/Src/ledFunctions.c
@@ -9,35 +9,29 @@ void setWhite (struct color* colors, int position, int percent)
 	return;
 }
 
-void setBlack (struct color* colors, int quantity, int percent)
+void setBlack (struct color* colors, int position, int percent)
 {
-	for(int i = 0; i < quantity; i++)
-	{
-		colors[i].green = 0;
-		colors[i].red = 0;
-		colors[i].blue = 0;
-	}
+		colors[position].green = 0;
+		colors[position].red = 0;
+		colors[position].blue = 0;
+
 	return;
 }
 
-void setBlue (struct color* colors, int quantity, int percent)
+void setBlue (struct color* colors, int position, int percent)
 {
-	for(int i = 0; i < quantity; i++)
-	{
-		colors[i].green = 0;
-		colors[i].red = 0;
-		colors[i].blue = 255*percent/100;
-	}
+		colors[position].green = 0;
+		colors[position].red = 0;
+		colors[position].blue = 255*percent/100;
+
 	return;
 }
 
-void setRed (struct color* colors, int quantity, int percent)
+void setRed (struct color* colors, int position, int percent)
 {
-	for(int i = 0; i < quantity; i++)
-	{
-		colors[i].green = 0;
-		colors[i].red = 255*percent/100;
-		colors[i].blue = 0;
-	}
+		colors[position].green = 0;
+		colors[position].red = 255*percent/100;
+		colors[position].blue = 0;
+
 	return;
 }
--- MiniProjectDraft2/Src/ledFunctions.h
+++ MiniProjectDraft2/Src/ledFunctions.h
@@ -10,8 +10,8 @@ void ledStripWrite(struct color* data, int numBytes);
 
 void setWhite (struct color* colors, int position, int percent);
 
-void setBlack (struct color* colors, int quantity, int percent);
+void setBlack (struct color* colors, int position, int percent);
 
-void setBlue (struct color* colors, int quantity, int percent);
+void setBlue (struct color* colors, int position, int percent);
 
-void setRed (struct color* colors, int quantity, int percent);
+void setRed (struct color* colors, int position, int percent);
