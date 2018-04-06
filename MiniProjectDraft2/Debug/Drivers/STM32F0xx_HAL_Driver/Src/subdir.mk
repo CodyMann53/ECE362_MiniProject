8c9a482dd3266878adedd26c853cd08e5821bf76
Cody Mann <mann53@purdue.edu>
2018-04-06 09:49:21 -0400

    I added display driver files to project.

--- MiniProjectDraft2/Debug/Drivers/STM32F0xx_HAL_Driver/Src/subdir.mk
+++ MiniProjectDraft2/Debug/Drivers/STM32F0xx_HAL_Driver/Src/subdir.mk
@@ -63,7 +63,7 @@ Drivers/STM32F0xx_HAL_Driver/Src/%.o: ../Drivers/STM32F0xx_HAL_Driver/Src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU GCC Compiler'
 	@echo $(PWD)
-	arm-none-eabi-gcc -mcpu=cortex-m0 -mthumb -mfloat-abi=soft '-D__weak=__attribute__((weak))' '-D__packed="__attribute__((__packed__))"' -DUSE_HAL_DRIVER -DSTM32F051x8 -I"C:/Users/Cody Mann/git/MiniProjectDraft2/Inc" -I"C:/Users/Cody Mann/git/MiniProjectDraft2/Drivers/STM32F0xx_HAL_Driver/Inc" -I"C:/Users/Cody Mann/git/MiniProjectDraft2/Drivers/STM32F0xx_HAL_Driver/Inc/Legacy" -I"C:/Users/Cody Mann/git/MiniProjectDraft2/Drivers/CMSIS/Device/ST/STM32F0xx/Include" -I"C:/Users/Cody Mann/git/MiniProjectDraft2/Drivers/CMSIS/Include"  -Og -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	arm-none-eabi-gcc -mcpu=cortex-m0 -mthumb -mfloat-abi=soft '-D__weak=__attribute__((weak))' '-D__packed="__attribute__((__packed__))"' -DUSE_HAL_DRIVER -DSTM32F051x8 -I"C:/Users/Cody Mann/git/ECE362_MiniProject/MiniProjectDraft2/Inc" -I"C:/Users/Cody Mann/git/ECE362_MiniProject/MiniProjectDraft2/Drivers/STM32F0xx_HAL_Driver/Inc" -I"C:/Users/Cody Mann/git/ECE362_MiniProject/MiniProjectDraft2/Drivers/STM32F0xx_HAL_Driver/Inc/Legacy" -I"C:/Users/Cody Mann/git/ECE362_MiniProject/MiniProjectDraft2/Drivers/CMSIS/Device/ST/STM32F0xx/Include" -I"C:/Users/Cody Mann/git/ECE362_MiniProject/MiniProjectDraft2/Drivers/CMSIS/Include"  -Og -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
